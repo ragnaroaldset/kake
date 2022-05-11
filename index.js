ad88abf6b95c1dadca7e985afe1b85661918ee63
Ragna Roaldset <ragna.roaldset@zegeba.com>
2022-05-11 08:46:21 +0200

    changed port again

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3050;
+const port = 5000;
 const path = require("path");
 const comments = require("./comments");
 const bodyParser = require("body-parser");
