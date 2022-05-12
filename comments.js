782b2ab42b8723af17ccd17fabd3a9ef9fe253a7
Ragna Roaldset <ragna.roaldset@zegeba.com>
2022-05-12 13:17:30 +0200

    added database

--- comments.js
+++ comments.js
@@ -1,34 +1,34 @@
-const fs = require('fs-extra');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require("express");
 const router = express.Router();
 
+const uri = `mongodb+srv://${process.env.DATABASE_USERNAME}:${process.env.DATABASE_PASSWORD}@cluster0.vb3eg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+client.connect();
+
 router.get("/comments", (req, res) => {
-	return fs.ensureFile("./comments.json")
-		.then(() => {
-			const currentComments = fs.readFileSync("comments.json", "utf-8");
-			const parsedCurrentComments = currentComments ? JSON.parse(currentComments) : [];
-			res.send(parsedCurrentComments);
+	const collection = client.db("sykkjanjmai").collection("comments");
+	const cursor = collection.find({})
+	return cursor.toArray()
+		.then(results => {
+			const formatted = results.map(doc => ({ comment: doc.comment, user: doc.user }));
+			res.send(JSON.stringify(formatted));
 		});
 });
 
 router.post("/comments", (req, res) => {
-	return fs.ensureFile("./comments.json")
-		.then(() => {
-			const currentComments = fs.readFileSync("comments.json", "utf-8");
-			const parsedCurrentComments = currentComments ? JSON.parse(currentComments) : [];
-
-			const {comment, user} = req.body;
-			parsedCurrentComments.push(req.body);
-			const newComments = JSON.stringify(parsedCurrentComments);
-			fs.writeFile('./comments.json', newComments, function (err) {
-				if (err) {
-					console.log('There has been an error saving your configuration data.');
-					console.log(err.message);
-					return;
-				}
-				console.log('Configuration saved successfully.')
+	const collection = client.db("sykkjanjmai").collection("comments");
+	const {comment, user} = req.body;
+	if (comment && user) {
+		return collection.insertOne(req.body)
+			.then(() => res.sendStatus(200))
+			.catch(err => {
+				res.status(400).send("an error occured when saving your comment");
 			});
-		})
+	} else {
+		res.sendStatus(400);
+	}
 });
 
 module.exports = router;
